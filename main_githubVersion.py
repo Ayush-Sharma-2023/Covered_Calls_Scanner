f0c012064a8b5c04757a7e009e28085efc2e2ca4
Ayush Sharma <Ayushsharmaarps@gmail.com>
2025-11-23 16:10:11 +0530

    minor changes

--- main_githubVersion.py
+++ main_githubVersion.py
@@ -2,24 +2,16 @@ import streamlit as st
 import pandas as pd
 import json
 
-# --- Load JSON ---
-with open("deploy.json", "r") as f:
-    content = json.load(f)
+with open("deploy.json", "r") as f:  content = json.load(f)
 
-# Extract timestamp and data
 current_time = content.get("last_fetched", "N/A")
 data = content.get("data", [])
 
-# Convert to DataFrame
 df = pd.DataFrame(data)
 
-# --- Streamlit App ---
 st.set_page_config(layout="wide", page_title="Covered Call Viewer")
 
 st.title("Covered Call Scanner")
-# st.info("Test")
-# st.context("TEST")
-# st.sidebar
 
 st.info(f"Data last fetched:   {current_time}")
 
